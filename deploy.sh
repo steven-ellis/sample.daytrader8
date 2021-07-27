7e168de575077010c0264e8c2241dea4c796ced9
Steven Ellis <steven.ellis@gmail.com>
2021-07-27 13:47:37 +1200

    Script the core of the deployment

---
+++ deploy.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# Deployment wrapper for the Legacy Day Trader application
+#
+# The Keycloak REST automation is based on guides from
+#  - https://dev.to/rounakcodes/keycloak-rest-api-for-realm-role-5hgp
+#  - https://suedbroecker.net/2021/07/16/upload-an-user-to-keycloak-using-curl/
+
+AUTH_REALM=stocktrader
+PROJECT_HOME=`pwd`
+OCP_NAMESPACE=daytrader
+
+# oc_wait_for 
+#
+# $1 = [pod|node]
+# $2 = app-name
+# $3 = [app|name|role] - defaults to app
+# $4 = namespace - defailts to ${OCP_NAMESPACE}
+#
+# EG
+#    oc_wait_for pod rook-ceph-mon
+#
+oc_wait_for ()
+{
+    TYPE=${3:-app}
+    NAMESPACE=${4:-$OCP_NAMESPACE}
+
+    echo "Waiting for the ${1}s tagged ${2} = ready"
+    oc wait --for condition=ready ${1} -l ${TYPE}=${2} -n ${NAMESPACE} --timeout=400s
+}
+
+
+# check_oc_login
+#
+# Make sure we're logged into OCP and grab our API endpoint
+#
+check_oc_login ()
+{
+    #OC_TOKEN=`oc whoami -t`
+    OCP_USER=`oc whoami | sed "s/://"`
+
+    OCP_ENDPOINT=`oc whoami --show-server`
+
+    if [ "${OCP_USER}" == "" ]; then
+      echo "You aren't logged into OpenShift at ${OCP_ENDPOINT}"
+      exit 1
+    else
+
+      echo "You are logged into OpenShift as $OCP_USER at ${OCP_ENDPOINT}"
+    fi
+}
+
+
+deploy_kafka ()
+{
+
+    oc apply -k k8s/kafka/prod
+}
+
+
+# We need to update the entries in the file
+
+
+deploy_database ()
+{
+    
+    
+    oc apply -k k8s/db/prod
+
+
+    # Need to implement a delay here while we wait for mysql to load
+    oc_wait_for  pod mysql  app 
+
+}
+
+deploy_apps ()
+{
+    
+
+    oc apply -k k8s/sampledaytrader8/prod
+
+    oc_wait_for pod sampledaytrader8   app 
+    
+    echo "20s delay while we wait for the service to come up"
+
+}
+
+# confirm_app_running
+#
+# Uses CURL to query that a webapp is running correctly
+#
+# $i = URL 
+#
+confirm_app_running ()
+{
+
+   CHECK_URL=${1}
+
+   for i in {1..12}
+   do
+      echo "checking for 200 status webapp $1 attempt $i"
+      status=`curl -o /dev/null -k -s -w "%{http_code}\n" ${CHECK_URL}`
+      if [ "${status}" == "200" ] ; then
+         echo "Application now available at at ${CHECK_URL}" >&2
+         return;
+      else
+         echo "got status ${status}"
+      fi
+      sleep 10s
+   done
+   echo "ERROR: Application at ${CHECK_URL} not in Running state" >&2
+   exit
+}
+
+
+populate_db ()
+{
+    export DAYTRADER_ROUTE="https://$(oc get route -n daytrader sampledaytrader8 -o jsonpath='{.spec.host}')"
+
+    echo "you can access the console via the following URL"
+    echo $DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/
+
+
+    confirm_app_running "$DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/"
+
+    curl -k -X GET "$DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/config?action=buildDBTables" | 
+
+
+    oc scale -n daytrader --replicas=0 deploy/sampledaytrader8
+    oc scale -n daytrader --replicas=1 deploy/sampledaytrader8
+    sleep 5s
+
+    oc_wait_for pod sampledaytrader8   app 
+
+    confirm_app_running "$DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/"
+    
+    echo "Now populate the Database"
+    curl -k -X GET "$DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/config?action=buildDB"
+
+    echo "If the populate DB phase fails we recommend you use the following URL"
+    echo $DAYTRADER_ROUTE/io.openliberty.sample.daytrader8/
+
+}
+
+
+deploy_debezium ()
+{
+
+    oc apply -k k8s/debezium/prod
+    
+    oc_wait_for pod daytrader-debezium-connect   strimzi.io/name
+
+
+}
+
+#check_oc_login
+
+#deploy_database
+#deploy_apps
+
+#populate_db 
+
+deploy_kafka
+deploy_debezium
+
+
